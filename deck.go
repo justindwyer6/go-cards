7a37849f75d2b5f19abd5fdcf07fa7217fc79465
Justin Dwyer <justindwyer6@gmail.com>
2021-01-16 19:41:38 +0000

    implementing the deal() function as built in Lesson 22, which is way nicer than what I threw together

--- deck.go
+++ deck.go
@@ -23,24 +23,9 @@ func newDeck() deck {
 func (d deck) print() {
 	for _, card := range d {
 		fmt.Println(card)
+		print("hi")
 	}
 }
-
-// func (d deck) deal() deck {
-// 	var newHand []string
-// 	for i, card := range d {
-// 		if i < 5 {
-// 			// append 1 card to newHand
-// 			newHand = append(newHand, card)
-// 			// remove that card from deck
-// 			// Taken from https://yourbasic.org/golang/delete-element-slice/
-// 			d[i] = d[len(d)-1]
-// 			d[len(d)-1] = ""
-// 			d = d[:len(d)-1]
-// 		}
-// 	}
-// 	return newHand
-// }
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
--- main.go
+++ main.go
@@ -5,12 +5,6 @@ import "fmt"
 func main() {
 	cards := newDeck()
 
-	// hand1 := deal()
-	// fmt.Println("Hand:")
-	// hand1.print()
-	// fmt.Println("Deck:")
-	// cards.print()
-
 	hand, cards := deal(cards, 5)
 	fmt.Println("Hand:")
 	hand.print()
