0210465dbd5ee5397961713b054cb4222f207ca8
Justin Dwyer <justindwyer6@gmail.com>
2021-01-18 15:03:49 +0000

    adding function to save to file

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -37,6 +38,6 @@ func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
 
-// func saveToFile(d deck) {
-// 	ioutil.WriteFile("Test Deck", d)
-// }
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
