c535b3a891fd5847ab6c95ba6d1fdcc6c1175a04
Justin Dwyer <justindwyer6@gmail.com>
2021-01-18 16:19:12 +0000

    adds test to check first card

--- deck_test.go
+++ deck_test.go
@@ -4,9 +4,14 @@ import "testing"
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
-	a := 16
 
-	if len(d) != a {
-		t.Errorf("Expected deck length of %v, but got %v", a, len(d))
+	lengthAssertion := 16
+	if len(d) != lengthAssertion {
+		t.Errorf("Expected deck length of %v, but got %v", lengthAssertion, len(d))
+	}
+
+	firstCardAssertion := "Ace of Spades"
+	if d[0] != firstCardAssertion {
+		t.Errorf("Expected first card to be %v, but got %v", firstCardAssertion, d[0])
 	}
 }
